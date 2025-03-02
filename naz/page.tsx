6a6d18fc0050a8f71a92c5fdf40977f39607f738
nazrulnazx <nazrulhassan2312@gmail.com>
2025-03-03 01:31:30 +0530

    Sample UI layouts

---
+++ naz/page.tsx
@@ -0,0 +1,332 @@
+'use client'
+import React, { useState } from "react";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+
+
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from '@tanstack/react-table'
+
+type Person = {
+    firstName: string
+    lastName: string
+    age: number
+    visits: number
+    status: string
+    progress: number
+}
+
+const defaultData: Person[] = [
+    {
+        firstName: 'tanner',
+        lastName: 'linsley',
+        age: 24,
+        visits: 100,
+        status: 'In Relationship',
+        progress: 50,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+    {
+        firstName: 'joe',
+        lastName: 'dirte',
+        age: 45,
+        visits: 20,
+        status: 'Complicated',
+        progress: 10,
+    },
+]
+
+const columnHelper = createColumnHelper<Person>()
+
+const columns = [
+    columnHelper.accessor('firstName', {
+        cell: info => info.getValue(),
+        header: () => <span>First Name</span>,
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor(row => row.lastName, {
+        id: 'lastName',
+        cell: info => <i>{info.getValue()}</i>,
+        header: () => <span>Last Name</span>,
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('age', {
+        header: () => 'Age',
+        cell: info => info.renderValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('visits', {
+        header: () => <span>Visits</span>,
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('status', {
+        header: 'Status',
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('progress', {
+        header: 'Profile Progress',
+        footer: info => info.column.id,
+    }),
+]
+
+export default function Page() {
+
+
+    const [data, _setData] = useState(() => [...defaultData])
+    // const rerender = React.useReducer(() => ({}), {})[1]
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+
+    const invoices = [
+        {
+            invoice: "INV001",
+            paymentStatus: "Paid",
+            totalAmount: "$250.00",
+            paymentMethod: "Credit Card",
+        },
+        {
+            invoice: "INV002",
+            paymentStatus: "Pending",
+            totalAmount: "$150.00",
+            paymentMethod: "PayPal",
+        },
+        {
+            invoice: "INV003",
+            paymentStatus: "Unpaid",
+            totalAmount: "$350.00",
+            paymentMethod: "Bank Transfer",
+        },
+        {
+            invoice: "INV004",
+            paymentStatus: "Paid",
+            totalAmount: "$450.00",
+            paymentMethod: "Credit Card",
+        },
+        {
+            invoice: "INV005",
+            paymentStatus: "Paid",
+            totalAmount: "$550.00",
+            paymentMethod: "PayPal",
+        },
+        {
+            invoice: "INV006",
+            paymentStatus: "Pending",
+            totalAmount: "$200.00",
+            paymentMethod: "Bank Transfer",
+        },
+        {
+            invoice: "INV007",
+            paymentStatus: "Unpaid",
+            totalAmount: "$300.00",
+            paymentMethod: "Credit Card",
+        },
+    ]
+
+    return (
+        <div className="flex flex-col gap-4 p-4 pt-0">
+            
+            <div className="flex-1 rounded-xl bg-muted/50 p-2">
+                <Table className="gap-4">
+
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+
+                    <TableHeader className="hover:rounded-xl">
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map(header => (
+                                    <TableHead key={header.id} className="w-[7000px]">
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+
+                    <TableBody>
+
+                        {table.getRowModel().rows.map(row => (
+                            <TableRow key={row.id}>
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+
+                    <TableFooter>
+                        {table.getFooterGroups().map(footerGroup => (
+                            <TableRow key={footerGroup.id}>
+                                {footerGroup.headers.map(header => (
+                                    <TableCell key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.footer,
+                                                header.getContext()
+                                            )}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableFooter>
+
+                </Table>
+            </div>
+
+            <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+            </div>
+
+            <div className="flex-1 rounded-xl bg-muted/50 p-2">
+                <Table className="gap-4">
+
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+
+                    <TableHeader className="hover:rounded-xl">
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map(header => (
+                                    <TableHead key={header.id} className="w-[7000px]">
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+
+                    <TableBody>
+
+                        {table.getRowModel().rows.map(row => (
+                            <TableRow key={row.id}>
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+
+                    <TableFooter>
+                        {table.getFooterGroups().map(footerGroup => (
+                            <TableRow key={footerGroup.id}>
+                                {footerGroup.headers.map(header => (
+                                    <TableCell key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.footer,
+                                                header.getContext()
+                                            )}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableFooter>
+
+                </Table>
+            </div>
+
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+            </div>
+
+            <div className="flex-1 rounded-xl bg-muted/50 p-2">
+                <Table className="gap-4">
+
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+
+                    <TableHeader className="hover:rounded-xl">
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map(header => (
+                                    <TableHead key={header.id} className="w-[7000px]">
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+
+                    <TableBody>
+
+                        {table.getRowModel().rows.map(row => (
+                            <TableRow key={row.id}>
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+
+                    <TableFooter>
+                        {table.getFooterGroups().map(footerGroup => (
+                            <TableRow key={footerGroup.id}>
+                                {footerGroup.headers.map(header => (
+                                    <TableCell key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.footer,
+                                                header.getContext()
+                                            )}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableFooter>
+
+                </Table>
+            </div>
+        </div>
+    )
+}
